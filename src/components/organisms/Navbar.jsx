164d3fd02cb2aa746c96cf25d577d4350596c1ce
Matheus Souza Rodrigues <matheussr2006@gmail.com>
2025-08-20 17:05:34 -0300

    initial commit

---
+++ src/components/organisms/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {  
+    return (
+        <div className="fixed flex item-center justify-between top-0 left-0 w-full px-30 py-5 bg-foreground">
+            <div className="flex items-center justify-center gap-15">
+                <img src="/3dburguer.png" alt="" className="size-15"/>
+                <h3 className="font-lexend font-bold text-2xl">CriaBurg</h3>
+            </div>
+            <div className="flex items-center justify-center gap-15">
+
+            </div>
+        </div>
+    )
+}
